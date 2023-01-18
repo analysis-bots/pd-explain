eae5e04a66be172997c27bb06cdb6e95b75ce99a
edenIsakov <31316799+edenIsakov@users.noreply.github.com>
2023-01-18 22:37:35 +0200

    change style

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,11 @@ intersphinx_mapping = {
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 
+intersphinx_disabled_domains = ['std']
+
+
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -38,4 +41,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-
+# -- Options for EPUB output
+epub_show_urls = 'footnote'

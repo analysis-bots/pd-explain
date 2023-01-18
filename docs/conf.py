9b69017f87aadf466778e74e808ade55d9bfcff8
edenIsakov <31316799+edenIsakov@users.noreply.github.com>
2023-01-18 23:35:20 +0200

    try to fix autofunction

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'pd_Explain'
 copyright = '2023, Eden Isakov, DR Amit Somech'

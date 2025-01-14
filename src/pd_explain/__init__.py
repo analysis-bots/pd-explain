4d04b5767138e0a4ac5119cd1f6d76d83d544a2c
Yuval Uner <yuvaluner@gmail.com>
2025-01-14 16:15:01 +0200

    Adjusted imports for tests to fit the new directory structure.

--- src/pd_explain/__init__.py
+++ src/pd_explain/__init__.py
@@ -3,6 +3,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from pd_explain.core.explainable_data_frame import ExpDataFrame
+from pd_explain.core.explainable_series import ExpSeries
 from pd_explain.utils.utils import to_explainable
 
 __version__ = '1.0.0'
